37860832d722a8d9b4af46fd408eacd2a04a6e0b
AndresAvila <avilabarre68@gmail.com>
2026-09-03 19:51:06 -0500

    Add files via upload

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -5,10 +5,7 @@
       <div class="logo">
         <h1>Catálogo Bulk</h1>
       </div>
-      <div class="nav-links">
-        <router-link to="/login" class="btn-login" v-if="!authStore.token">Iniciar Sesión</router-link>
-        <router-link to="/admin/productos" class="btn-login" v-else>Panel Admin</router-link>
-      </div>
+      
     </header>
 
     <!-- Sección Hero / Banner Principal -->
@@ -485,7 +482,6 @@ onMounted(() => {
   font-size: 0.85rem;
   margin-bottom: 1rem;
   display: -webkit-box;
-  -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
